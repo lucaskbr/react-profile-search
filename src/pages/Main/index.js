9d6c3d2c1285d57514d087a519825de266087477
lucaskbr <lucas_klasa@hotmail.com>
2019-10-05 18:53:40 -0300

    Project structure

---
+++ src/pages/Main/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+function Main() {
+  return <View />;
+}
+
+Main.navigationOptions = {
+  title: 'Usuários',
+};
+
+export default Main;
